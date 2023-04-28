6b8a13a461213e22ba7be262d17b21aeddc62ca5
newm1n <fbsns20@gmail.com>
2023-04-28 22:31:25 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express(); // 執行 Express
 const port = 3000;
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", () => {
+  console.log("mongodb error!");
+});
+db.once("open", () => {
+  console.log("mongodb connected!");
+});
 
 app.get("/", (req, res) => {
   res.send("Homepage.");
