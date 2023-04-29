22b5278ce81eebdc0df657757da49a8b24f93ff1
newm1n <fbsns20@gmail.com>
2023-04-29 18:09:52 +0800

    refactor: modify mongoose config and fix main.hbs

--- app.js
+++ app.js
@@ -1,32 +1,16 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
 const routes = require("./routes");
-const app = express(); // 執行 Express
+require("./config/mongoose");
+
+const app = express();
 const port = 3000;
-const Restaurant = require("./models/restaurant");
-
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
-
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-const db = mongoose.connection;
-db.on("error", () => {
-  console.log("mongodb error!");
-});
-db.once("open", () => {
-  console.log("mongodb connected!");
-});
 
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", "hbs");
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(routes);
