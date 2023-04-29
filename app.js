486801b81aed11c482ea7bacd6783f3d9f5e593b
newm1n <fbsns20@gmail.com>
2023-04-29 15:24:48 +0800

    feat: add handlebars and modify main & index.hbs

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
 const app = express(); // 執行 Express
 const port = 3000;
+const Restaurant = require("./models/restaurant");
 
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
@@ -20,8 +24,16 @@ db.once("open", () => {
   console.log("mongodb connected!");
 });
 
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", "hbs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
 app.get("/", (req, res) => {
-  res.send("Homepage.");
+  Restaurant.find()
+    .lean()
+    .then((restaurantsData) => res.render("index", { restaurantsData }))
+    .catch((err) => console.log(err));
 });
 
 app.listen(port, () => {
