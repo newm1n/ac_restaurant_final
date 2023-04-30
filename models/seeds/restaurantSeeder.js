20a4c7415b6aa1b991c4e2aee0901abac421edc8
newm1n <fbsns20@gmail.com>
2023-04-30 22:22:54 +0800

    modify todoSeeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,27 +1,60 @@
-const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const db = require("../../config/mongoose");
 const Restaurant = require("../restaurant");
 const restaurantList = require("../restaurant.json").results;
+const User = require("../user");
 
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
+const SEED_USER = [
+  {
+    name: "user1",
+    email: "user1@example.com",
+    password: "12345678",
+    restaurantIndex: [0, 1, 2],
+  },
+  {
+    name: "user2",
+    email: "user2@example.com",
+    password: "12345678",
+    restaurantIndex: [3, 4, 5],
+  },
+];
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-const db = mongoose.connection;
-db.on("error", () => {
-  console.log("mongodb error!");
-});
 db.once("open", () => {
-  console.log("running restaurantSeeder script...");
-
-  Restaurant.create(restaurantList)
+  bcrypt
+    .genSalt(10)
+    .then((salt) => bcrypt.hash(SEED_USER[0].password, salt))
+    .then((hash) => {
+      return Promise.all(
+        Array.from({ length: SEED_USER.length }, (_, i) =>
+          User.create({
+            name: SEED_USER[i].name,
+            email: SEED_USER[i].email,
+            password: hash,
+          })
+        )
+      );
+    })
+    .then((users) => {
+      const userId = users.map((user) => user._id);
+      const restaurants = [];
+      Array.from(SEED_USER, (user, index) => {
+        Array.from(user.restaurantIndex, (index) => {
+          const restaurant = {
+            ...restaurantList[index],
+            userId: userId[SEED_USER.indexOf(user)],
+          };
+          restaurants.push(restaurant);
+        });
+      });
+      return Promise.all(
+        Array.from({ length: restaurants.length }, (_, i) =>
+          Restaurant.create(restaurants[i])
+        )
+      );
+    })
     .then(() => {
-      console.log("restaurantSeeder done!");
-      db.close();
+      console.log("Seeder done.");
+      process.exit();
     })
     .catch((err) => console.log(err));
 });
