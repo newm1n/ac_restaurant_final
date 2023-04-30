debfb6d3645cac8e8eabe6c3053a5b6a33115ccd
newm1n <fbsns20@gmail.com>
2023-04-30 15:30:39 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const session = require("express-session");
 const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
 const routes = require("./routes");
+
+const usePassport = require("./config/passport");
 require("./config/mongoose");
 
 const app = express();
@@ -10,9 +13,21 @@ const port = 3000;
 
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", "hbs");
+
+app.use(
+  session({
+    secret: "ThisIsMySecret",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+
+usePassport(app);
+
 app.use(routes);
 
 app.listen(port, () => {
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../../models/user");
+const passport = require("passport");
 
 router.get("/login", (req, res) => {
   res.render("login");
 });
 
-router.post("/login", (req, res) => {});
+// 加入 middleware，驗證 request 登入狀態
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/users/login",
+  })
+);
 
 router.post("/register", (req, res) => {
   // 取得註冊表單參數
